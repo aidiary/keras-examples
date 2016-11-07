b757736c21e6a158deeb996df89e3e3c96a0323d
aidiary <f2forest@gmail.com>
2016-11-07 17:43:56 +0900

    Add plotting model

--- mlp/mnist.py
+++ mlp/mnist.py
@@ -7,10 +7,25 @@ from keras.layers.core import Dense, Dropout, Activation
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 from keras.utils import np_utils
+from keras.utils.visualize_util import plot
 
 # MNISTの数字分類
 # 参考
 # https://github.com/fchollet/keras/blob/master/examples/mnist_mlp.py
+
+def build_multilayer_perceptron():
+    model = Sequential()
+
+    model.add(Dense(512, input_shape=(784,)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(10))
+    model.add(Activation('softmax'))
+
+    return model
 
 def plot_history(history):
     # print(history.history.keys())
@@ -59,18 +74,11 @@ if __name__ == "__main__":
     Y_test = np_utils.to_categorical(y_test, nb_classes)
 
     # 多層ニューラルネットワークモデルを構築
-    model = Sequential()
-    model.add(Dense(512, input_shape=(784,)))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(512))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(10))
-    model.add(Activation('softmax'))
+    model = build_multilayer_perceptron()
 
     # モデルのサマリを表示
     model.summary()
+    plot(model, show_shapes=True, show_layer_names=True, to_file='model.png')
 
     # Early-stopping
     early_stopping = EarlyStopping()
