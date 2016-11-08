b1e8955f6093afe5dd74ac5e461242b5ded0005d
aidiary <f2forest@gmail.com>
2016-11-08 18:43:34 +0900

    Refactoring

--- cnn/mnist.py
+++ cnn/mnist.py
@@ -8,10 +8,35 @@ from keras.layers import Convolution2D, MaxPooling2D
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 from keras.utils import np_utils
+from keras import backend as K
 
 # MNISTの数字分類
-# 参考
-# https://github.com/fchollet/keras/blob/master/examples/mnist_mlp.py
+# 参考: https://github.com/fchollet/keras/blob/master/examples/mnist_cnn.py
+
+def build_cnn(input_shape, nb_filters, kernel_size, pool_size):
+    model = Sequential()
+
+    model.add(Convolution2D(nb_filters,
+                            kernel_size[0], kernel_size[1],
+                            border_mode='valid',
+                            input_shape=input_shape))
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D(pool_size=pool_size))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+
+    return model
 
 def plot_history(history):
     # print(history.history.keys())
@@ -48,10 +73,15 @@ if __name__ == "__main__":
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
     # 画像集合を表す4次元テンソルに変形
-    # keras.jsonのimage_dim_orderingがtfのときはチャネルは4次元目にくるので注意
-    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
-    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+    # keras.jsonのimage_dim_orderingがthのときはチャネルが2次元目、tfのときはチャネルが4次元目にくる
+    if K.image_dim_ordering() == 'th':
+        X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+        X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+        input_shape = (1, img_rows, img_cols)
+    else:
+        X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+        X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
 
     # 画素を0.0-1.0の範囲に変換
     X_train = X_train.astype('float32')
@@ -67,42 +97,26 @@ if __name__ == "__main__":
     Y_test = np_utils.to_categorical(y_test, nb_classes)
 
     # 畳み込みニューラルネットワークを構築
-    model = Sequential()
-    model.add(Convolution2D(nb_filters,
-                            kernel_size[0], kernel_size[1],
-                            border_mode='valid',
-                            input_shape=input_shape))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=pool_size))
-    model.add(Dropout(0.25))
-
-    model.add(Flatten())
-    model.add(Dense(128))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(nb_classes))
-    model.add(Activation('softmax'))
+    model = build_cnn(input_shape, nb_filters, kernel_size, pool_size)
 
     # モデルのサマリを表示
     model.summary()
-
-    # Early-stopping
-    early_stopping = EarlyStopping()
 
     # モデルをコンパイル
     model.compile(loss='categorical_crossentropy',
                   optimizer='adam',
                   metrics=['accuracy'])
 
+    # Early-stopping
+    early_stopping = EarlyStopping()
+
     # モデルの訓練
     history = model.fit(X_train, Y_train,
-                        batch_size=batch_size,
-                        nb_epoch=nb_epoch,
-                        verbose=1,
-                        validation_data=(X_test, Y_test),
-                        callbacks=[early_stopping])
+                       batch_size=batch_size,
+                       nb_epoch=nb_epoch,
+                       verbose=1,
+                       validation_split=0.1,
+                       callbacks=[early_stopping])
 
     # 学習履歴をプロット
     plot_history(history)
