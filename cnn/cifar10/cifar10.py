c41d937f9f548ccf926c4d31dbf428194c5e7774
aidiary <f2forest@gmail.com>
2016-12-09 10:29:12 +0900

    Remove unused import

--- cnn/cifar10/cifar10.py
+++ cnn/cifar10/cifar10.py
@@ -8,7 +8,6 @@ from keras.datasets import cifar10
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
-from keras.optimizers import SGD
 from keras.utils import np_utils
 from keras.utils.visualize_util import plot
 from keras.preprocessing.image import ImageDataGenerator
