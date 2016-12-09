e9a43a9b4fab78cc2a57550e9c7a0b5179160f32
aidiary <f2forest@gmail.com>
2016-12-09 11:46:01 +0900

    Modify to make test dataset

--- cnn/17flowers/split_images.py
+++ cnn/17flowers/split_images.py
@@ -1,10 +1,16 @@
 import os
 import shutil
+import random
 
 IN_DIR = 'jpg'
-OUT_DIR = 'images'
-if not os.path.exists(OUT_DIR):
-    os.mkdir(OUT_DIR)
+TRAIN_DIR = 'train_images'
+TEST_DIR = 'test_images'
+
+if not os.path.exists(TRAIN_DIR):
+    os.mkdir(TRAIN_DIR)
+
+if not os.path.exists(TEST_DIR):
+    os.mkdir(TEST_DIR)
 
 # name => (start idx, end idx)
 flower_dics = {}
@@ -24,7 +30,8 @@ with open('labels.txt') as fp:
 
 # 花ごとのディレクトリを作成
 for name in flower_dics:
-    os.mkdir(os.path.join(OUT_DIR, name))
+    os.mkdir(os.path.join(TRAIN_DIR, name))
+    os.mkdir(os.path.join(TEST_DIR, name))
 
 # jpgをスキャン
 for f in sorted(os.listdir(IN_DIR)):
@@ -36,6 +43,15 @@ for f in sorted(os.listdir(IN_DIR)):
         start, end = flower_dics[name]
         if idx in range(start, end + 1):
             source = os.path.join(IN_DIR, f)
-            dest = os.path.join(OUT_DIR, name)
+            dest = os.path.join(TRAIN_DIR, name)
             shutil.copy(source, dest)
             continue
+
+# 訓練データの各ディレクトリからランダムに10枚をテストとする
+for d in os.listdir(TRAIN_DIR):
+    files = os.listdir(os.path.join(TRAIN_DIR, d))
+    random.shuffle(files)
+    for f in files[:10]:
+        source = os.path.join(TRAIN_DIR, d, f)
+        dest = os.path.join(TEST_DIR, d)
+        shutil.move(source, dest)
