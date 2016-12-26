e64425594dafc024779960ee46232b91f2e03139
aidiary <f2forest@gmail.com>
2016-12-26 17:25:39 +0900

    Add the result of smallcnn.py

--- vgg16/17flowers/smallcnn.py
+++ vgg16/17flowers/smallcnn.py
@@ -21,8 +21,8 @@ def save_history(history, result_file):
 
 
 if __name__ == '__main__':
-    nb_epoch = 200
-    result_dir = 'result'
+    nb_epoch = 50
+    result_dir = 'results'
 
     print('nb_epoch:', nb_epoch)
     print('result_dir:', result_dir)
@@ -38,32 +38,30 @@ if __name__ == '__main__':
     batch_size = 32
     nb_classes = len(classes)
 
-    img_rows, img_cols = 64, 64
+    img_rows, img_cols = 150, 150
     channels = 3
+
+    samples_per_epoch = 1190
+    nb_val_samples = 170
 
     # CNNを構築
     model = Sequential()
-
-    model.add(Convolution2D(32, 3, 3, border_mode='same',
-                            input_shape=(img_rows, img_cols, channels)))
+    model.add(Convolution2D(32, 3, 3, input_shape=(img_rows, img_cols, channels)))
     model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Convolution2D(32, 3, 3))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
 
-    model.add(Convolution2D(64, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
     model.add(Convolution2D(64, 3, 3))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
 
     model.add(Flatten())
-    model.add(Dense(512))
+    model.add(Dense(64))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
-
     model.add(Dense(nb_classes))
     model.add(Activation('softmax'))
 
@@ -75,41 +73,42 @@ if __name__ == '__main__':
     model.summary()
     plot(model, show_shapes=True, to_file=os.path.join(result_dir, 'model.png'))
 
-    if not os.path.exists('train_gen'):
-        os.mkdir('train_gen')
-
     # ディレクトリの画像を使ったジェネレータ
-    # データ拡張は行わない
-    train_datagen = ImageDataGenerator()
+    train_datagen = ImageDataGenerator(
+        rescale=1.0 / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+    test_datagen = ImageDataGenerator(rescale=1.0 / 255)
+
     train_generator = train_datagen.flow_from_directory(
         directory='train_images',
         target_size=(img_rows, img_cols),
         color_mode='rgb',
         classes=classes,
         class_mode='categorical',
-        batch_size=32,
+        batch_size=batch_size,
         shuffle=True)
-        # save_to_dir='train_gen',
-        # save_format='jpg')
 
-    test_datagen = ImageDataGenerator()
     test_generator = test_datagen.flow_from_directory(
         directory='test_images',
         target_size=(img_rows, img_cols),
         color_mode='rgb',
         classes=classes,
         class_mode='categorical',
-        batch_size=32,
+        batch_size=batch_size,
         shuffle=True)
 
-    history = model.fit_generator(train_generator,
-                                  samples_per_epoch=17 * 70,
-                                  nb_epoch=nb_epoch,
-                                  validation_data=test_generator,
-                                  nb_val_samples=17 * 10)
+    history = model.fit_generator(
+        train_generator,
+        samples_per_epoch=samples_per_epoch,
+        nb_epoch=nb_epoch,
+        validation_data=test_generator,
+        nb_val_samples=nb_val_samples)
 
-    save_history(history, os.path.join(result_dir, 'history.txt'))
+    save_history(history, os.path.join(result_dir, 'history_smallcnn.txt'))
 
-    loss, acc = model.evaluate_generator(test_generator, val_samples=17 * 10)
+    loss, acc = model.evaluate_generator(test_generator, val_samples=800)
     print('Test loss:', loss)
     print('Test acc:', acc)
