0d960c6054b3f1b0a636c3a69283beaca50723e7
aidiary <f2forest@gmail.com>
2017-01-06 14:14:49 +0900

    Refactoring

--- vgg16/dogs_vs_cats/plot_results.py
+++ vgg16/dogs_vs_cats/plot_results.py
@@ -3,10 +3,13 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-result_file1 = os.path.join('results', 'history_smallcnn.txt')
-result_file2 = os.path.join('results', 'history_extractor.txt')
-result_file3 = os.path.join('results', 'history_finetuning.txt')
-result_file4 = os.path.join('results', 'history_finetuning_from_random.txt')
+result_dir = 'results'
+
+result_file1 = os.path.join(result_dir, 'history_smallcnn.txt')
+result_file2 = os.path.join(result_dir, 'history_extractor.txt')
+result_file3 = os.path.join(result_dir, 'history_finetuning.txt')
+result_file4 = os.path.join(result_dir, 'history_finetuning_from_random.txt')
+
 
 def load_results(filename):
     epoch_list = []
@@ -33,6 +36,7 @@ def load_results(filename):
 
     return epoch_list, val_loss_list, val_acc_list
 
+
 epoch1, val_loss1, val_acc1 = load_results(result_file1)
 epoch2, val_loss2, val_acc2 = load_results(result_file2)
 epoch3, val_loss3, val_acc3 = load_results(result_file3)
@@ -41,24 +45,24 @@ epoch4, val_loss4, val_acc4 = load_results(result_file4)
 plt.figure()
 plt.plot(epoch1, val_loss1, 'b-', marker='.', label='smallcnn')
 plt.plot(epoch2, val_loss2, 'r-', marker='.', label='extractor')
-plt.plot(epoch3, val_loss3, 'g-', marker='.', label='fine-tuning')
-plt.plot(epoch4, val_loss4, 'g--', marker='.', label='fine-tuning (random)')
+plt.plot(epoch3, val_loss3, 'g-', marker='.', label='finetuning')
+plt.plot(epoch4, val_loss4, 'g--', marker='.', label='finetuning (random)')
 plt.grid()
 plt.legend()
 plt.xlabel('epoch')
 plt.ylabel('val_loss')
 # plt.show()
-plt.savefig('val_loss.png')
+plt.savefig(os.path.join(result_dir, 'val_loss.png'))
 
 plt.figure()
 plt.plot(epoch1, val_acc1, 'b-', marker='.', label='smallcnn')
 plt.plot(epoch2, val_acc2, 'r-', marker='.', label='extractor')
-plt.plot(epoch3, val_acc3, 'g-', marker='.', label='fine-tuning')
-plt.plot(epoch4, val_acc4, 'g--', marker='.', label='fine-tuning (random)')
+plt.plot(epoch3, val_acc3, 'g-', marker='.', label='finetuning')
+plt.plot(epoch4, val_acc4, 'g--', marker='.', label='finetuning (random)')
 plt.grid()
 plt.legend(loc='lower right')
 plt.xlabel('epoch')
 plt.ylabel('val_acc')
 plt.ylim((0, 1))
 # plt.show()
-plt.savefig('val_acc.png')
+plt.savefig(os.path.join(result_dir, 'val_acc.png'))
