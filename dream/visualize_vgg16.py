3f1ef91fcdb51091861c597a932532567ee70e26
aidiary <f2forest@gmail.com>
2017-02-07 13:38:40 +0900

    Update visualize_vgg16.py

--- dream/visualize_vgg16.py
+++ dream/visualize_vgg16.py
@@ -43,22 +43,23 @@ def visualize_filter(layer_name, filter_index):
         print("ERROR: invalid filter index: %d" % filter_index)
         return
 
-    # 指定した層の指定したフィルタの出力の平均を損失とする
-    # 実際は、層の出力を最大化したいため損失は負の記号をつける
+    # 指定した層の指定したフィルタの出力の平均
+    # TODO: 損失は通常最小化を目指すものなので別の名前か負記号をつけた方がよい？
     if layer_name == 'predictions':
-        loss = - K.mean(layer.output[:, filter_index])
+        loss = K.mean(layer.output[:, filter_index])
     else:
-        loss = - K.mean(layer.output[:, :, :, filter_index])
+        loss = K.mean(layer.output[:, :, :, filter_index])
 
     # 層の出力の入力画像に対する勾配を求める
     # 入力画像を微小量変化させたときの出力の変化量を意味する
+    # 層の出力を最大化したいためこの勾配を画像に足し込む
     grads = K.gradients(loss, input_tensor)[0]
 
     # 正規化トリック
     # 画像に勾配を足し込んだときにちょうどよい値になる
     grads /= (K.sqrt(K.mean(K.square(grads))) + K.epsilon())
 
-    # 画像を入力して損失と勾配を返す関数を定義
+    # 画像を入力して層の出力と勾配を返す関数を定義
     iterate = K.function([input_tensor], [loss, grads])
 
     # ノイズを含んだ画像（4Dテンソル）から開始する
@@ -66,12 +67,25 @@ def visualize_filter(layer_name, filter_index):
     x = (x - 0.5) * 20 + 128
 
     # 初期画像を描画
+    # img = deprocess_image(x[0])
+    # plt.imshow(img)
+    # plt.show()
+
+    # 勾配法で損失を小さくするように入力画像を更新する
+    weight = 1.0
+    for i in range(200):
+        loss_value, grads_value = iterate([x])
+        # loss_valueを大きくしたいので画像に勾配を加える
+        x += weight * grads_value
+        print(i, loss_value)
+
     img = deprocess_image(x[0])
     plt.imshow(img)
     plt.show()
 
 
 if __name__ == '__main__':
-    visualize_filter('block1_conv1', 0)
-    visualize_filter('block5_conv3', 501)
+    # visualize_filter('block1_conv1', 0)
+    # visualize_filter('block5_conv3', 501)
+    visualize_filter('predictions', 20)
     visualize_filter('predictions', 64)
