ddd0112db51e6c214e0a677e815a3be07c59e89b
aidiary <f2forest@gmail.com>
2017-03-02 17:25:47 +0900

    Update dream1.py

--- dream/dream1.py
+++ dream/dream1.py
@@ -1,6 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from keras import backend as K
+from keras.layers import Input
 from keras.applications.inception_v3 import InceptionV3
+
+
+# ランダム画像から始める
+img_noise = np.random.uniform(size=(1, 299, 299, 3)) + 100.0
+# draw_image(img_noise)
+
+# input_tensorはバッチを入れない3Dテンソル
+input_tensor = Input(shape=(299, 299, 3))
+model = InceptionV3(include_top=True, weights='imagenet', input_tensor=input_tensor)
+layer_dict = dict([(layer.name, layer) for layer in model.layers])
+
 
 def draw_image(img):
     fig, ax = plt.subplots()
@@ -10,22 +23,27 @@ def draw_image(img):
     plt.show()
 
 
-# ランダム画像から始める
-img_noise = np.random.uniform(size=(224, 224, 3)) + 100.0
-# draw_image(img_noise)
+def render_naive(layer_name, filter_index, img0=img_noise, iter_n=20, step=1.0):
+    if layer_name not in layer_dict:
+        print("ERROR: invalid layer name: %s" % layer_name)
+        return
 
+    layer = layer_dict[layer_name]
 
-def render_naive(target_output, img0=img_noise, iter_n=20, step=1.0):
-    print(target_output)
+    print("{} < {}".format(filter_index, layer.output_shape[-1]))
 
+    activation = K.mean(layer.output[:, :, :, filter_index])
+    grads = K.gradients(activation, input_tensor)[0]
 
+    # DropoutやBNを含むネットワークはK.learning_phase()が必要
+    iterate = K.function([input_tensor, K.learning_phase()], [activation, grads])
 
+    img = img0.copy()
+    for i in range(iter_n):
+        # 学習はしないので0を入力
+        activation_value, grads_value = iterate([img, 0])
+        grads_value /= K.std(grads_value) + 1e-8
+        img += grads_value * step
+        print(i, activation_value)
 
-
-model = InceptionV3(weights='imagenet', include_top=True)
-layer_dict = dict([(layer.name, layer) for layer in model.layers])
-print(len(layer_dict))
-
-
-target_layer = 'convolution2d_40'
-target_channel = 139
+render_naive('convolution2d_40', 139)
